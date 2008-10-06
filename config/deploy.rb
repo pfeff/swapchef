7ee1c8eea1bd262df7478f90839570d4e6cbb733
pfeff <matthew.pfefferle@gmail.com>
2008-10-06 18:24:27 -0400

    Added a capistrano spin file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "swapchef.com"
+set :user, "dachef"
+set :repository,  "git@github.com:pfeff/swapchef.git"
+
+set :use_sudo, false
+set :deploy_via, :copy
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/dachef/public_html/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+set :scm, :git
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+#set :location, "swapchef.com"
+role :app, application
+role :web, application
+role :db,  application, :primary => true
